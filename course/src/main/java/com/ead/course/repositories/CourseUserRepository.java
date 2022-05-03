babda49dad96a01675be274f10615d073b2cec1a
Alex Freitag <devfreitag@gmail.com>
2022-05-03 13:00:59 -0300

    Delete courseUser relation before delete course

--- course/src/main/java/com/ead/course/repositories/CourseUserRepository.java
+++ course/src/main/java/com/ead/course/repositories/CourseUserRepository.java
@@ -3,8 +3,10 @@ package com.ead.course.repositories;
 import com.ead.course.models.CourseUserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CourseUserRepository extends JpaRepository<CourseUserModel, UUID> {
     boolean existsByCourseIdAndUserId(UUID courseId, UUID userId);
+    List<CourseUserModel> findAllByCourseId(UUID courseId);
 }
